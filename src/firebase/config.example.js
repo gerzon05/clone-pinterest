9fa70c9a9eb3d41a274486b92f6255fbdf15a844
Gerzon05 <gerzonarletrangel@gmail.com>
2024-04-25 21:47:27 -0500

    fix: ignore config.js

---
+++ src/firebase/config.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'XXXXXXXXX',
+  authDomain: 'XXXXXXXXX.firebaseapp.com',
+  projectId: 'XXXXXXXXXXXX',
+  storageBucket: 'XXXXXXXXXXXX.appspot.com',
+  messagingSenderId: 'XXXXXXXXXXXX',
+  appId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+}
