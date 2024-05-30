3f6be7432e64148b112419be0067e5176ea9b37e
Gerzon05 <gerzonarletrangel@gmail.com>
2024-05-29 20:51:23 -0500

    feat: optimize Pin component for better performance and user experience

--- src/modules/pin/Pin.tsx
+++ src/modules/pin/Pin.tsx
@@ -3,17 +3,21 @@ import { useLocation } from 'wouter'
 import { StateImage } from '@/store/hookimage'
 import { UseGetImage } from '@/hooks/useGetImage'
 import { UseSaveImage } from '@/hooks/useSaveImage'
+import { UserState } from '@/store/user'
 
 interface PinProps {
   filter?: string
 }
 
 export function Pin({ filter }: PinProps) {
+  const user = UserState(state => state.user)
   const [_, setLocation] = useLocation()
   const { setImage } = StateImage()
 
+  const email = (user as ({ email: string })).email
+
   const handleSaveImage = (url: string) => {
-    UseSaveImage({ url })
+    UseSaveImage({ url, email })
   }
 
   const { photos, loading } = UseGetImage({ filter })
@@ -33,32 +37,30 @@ export function Pin({ filter }: PinProps) {
         </div>
       )}
       {photos.map((photo, index) => (
-        <div key={index} onClick={() => handleImage(photo)}>
+        <div key={index}>
           <div className="group py-2 cursor-zoom-in relative overflow-hidden">
             <img
               src={(photo as { imagen: string }).imagen}
               className="w-full aspect-auto object-cover rounded-xl"
               alt={(photo as { titulo: string }).titulo}
             />
-            <span className="hidden absolute md:flex flex-col justify-between my-2 z-10 top-0 w-full bottom-0 bg-transparent pointer-events-none rounded-xl group-hover:bg-black/50 group-hover:pointer-events-auto ">
-              <article className="flex justify-end p-3 opacity-0 group-hover:opacity-100">
-                <button
-                  onClick={() =>
-                    handleSaveImage((photo as { imagen: string }).imagen)}
-                  className="py-1 px-4 bg-red-700 text-base rounded-full text-white"
-                >
-                  Guardar
-                </button>
-              </article>
-              <article className="flex gap-2 justify-end p-3 opacity-0 group-hover:opacity-100">
-                <div className="cursor-pointer w-7 h-7 bg-slate-300 rounded-full flex justify-center items-center">
-                  <Share className=" text-3xl" />
-                </div>
-                <div className="cursor-pointer w-7 h-7 bg-slate-300 rounded-full flex justify-center items-center">
-                  <CircleEllipsis className=" text-3xl" />
-                </div>
-              </article>
-            </span>
+            <button
+              onClick={() =>
+                handleSaveImage((photo as { imagen: string }).imagen)}
+              className="py-1 px-4 bg-red-700 text-base absolute top-4 right-2 rounded-full z-40 text-white opacity-0 group-hover:opacity-100"
+            >
+              Guardar
+            </button>
+            <article className="absolute z-40 right-2 bottom-4 opacity-0 group-hover:opacity-100 flex gap-2">
+              <button className="cursor-pointer w-7 h-7 bg-slate-300 rounded-full flex justify-center items-center">
+                <Share className=" text-3xl" />
+              </button>
+              <button className="cursor-pointer w-7 h-7 bg-slate-300 rounded-full flex justify-center items-center">
+                <CircleEllipsis className=" text-3xl" />
+              </button>
+            </article>
+            <button className="hidden absolute md:flex flex-col justify-between my-2 z-10 top-0 w-full bottom-0 bg-transparent pointer-events-none rounded-xl group-hover:bg-black/50 group-hover:pointer-events-auto" onClick={() => handleImage(photo)}>
+            </button>
           </div>
         </div>
       ))}
